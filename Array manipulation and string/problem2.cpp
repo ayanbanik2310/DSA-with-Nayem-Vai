7c519cc53ea896a4c732e3090cab86a1132ad317
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-09-26 01:06:39 +0600

    problem2.cpp

--- Array manipulation and string/problem2.cpp
+++ Array manipulation and string/problem2.cpp
@@ -1,1 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    int freq[26] = {0};
+
+    int i = 0;
+    while(s[i] != '\0')
+    {   
+        if(s[i] != ' ')  freq[s[i]-'a']++;  
+        i++; 
+    }
+
+    for(int i=0; i<26; i++)
+    {
+        if(freq[i] != 0)
+        {
+            cout<<char(i+'a')<<" ";
+            cout<<freq[i]<<endl;
+        }
+    }
+    
+    return 0;
+}
