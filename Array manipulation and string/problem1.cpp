3a03e5993fbb2aac651b776e8d84d2c246d3ff61
ayanbanik <115443390+ayanbanik2310@users.noreply.github.com>
2024-09-26 12:51:32 +0600

    problem1.cpp

--- Array manipulation and string/problem1.cpp
+++ Array manipulation and string/problem1.cpp
@@ -7,26 +7,49 @@ int main()
     string s;
     getline(cin, s);
 
+    vector<int>v;
 
     int i = 0; 
     int len = 0;
+    int mxheight = 0;
     while(s[i] != '\0')
     {   
         if(s[i] == ' ')
         {
-            cout<<" "<<len<<endl;
+            v.push_back(len);
+            mxheight = max(mxheight, len);
             len = 0;
             i++;
         }
         else
         {
-            cout<<s[i];
             len++;
             i++;
         }
 
     }
-    cout<<" "<<len;
+
+    mxheight = max(mxheight, len);
+    v.push_back(len);
+
+
+    for(int height = mxheight; height>0; height--)
+    {
+        for(int j = 0; j<v.size(); j++)
+        {
+            if(height <= v[j])
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    for(auto it:v) cout<<it<<" ";
+
 
     
     return 0;
